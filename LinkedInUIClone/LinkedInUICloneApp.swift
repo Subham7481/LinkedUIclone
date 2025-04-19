4fb9a60df0cc31af72b2e324169c246ce0244a3c
Subham Kaumar <patelsk7481@gmail.com>
2025-04-19 18:35:00 +0530

    Initial Commit

---
+++ LinkedInUIClone/LinkedInUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInUICloneApp.swift
+//  LinkedInUIClone
+//
+//  Created by Subham Patel on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinkedInUICloneTests/LinkedInUICloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInUICloneTests.swift
+//  LinkedInUICloneTests
+//
+//  Created by Subham Patel on 19/04/25.
+//
+
+import Testing
+@testable import LinkedInUIClone
+
+struct LinkedInUICloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
